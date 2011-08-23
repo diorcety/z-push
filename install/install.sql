0e13135d4a401e4dab9127f80d880bf1bfa5f306
Yann Diorcet <diorcet.yann@gmail.com>
2011-08-23 23:21:29 +0200

    Add tables to DB

--- install/install.sql
+++ install/install.sql
@@ -1,13 +1,86 @@
 
 
 -- -----------------------------------------------------
--- Table `sync_devices`
+-- Table `as_devices`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `as_devices`;
-CREATE  TABLE IF NOT EXISTS `as_devices` (
+CREATE TABLE IF NOT EXISTS `as_devices` (
   `user_id` INT(11) NOT NULL ,
   `device_id` VARCHAR(64) NOT NULL ,
   `policy_key` INT NOT NULL DEFAULT 0,
   `status` INT NOT NULL DEFAULT 1,
-  PRIMARY KEY (`user_id`, `device_id`) 
-) ENGINE = MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`user_id`, `device_id`),
+  FOREIGN KEY (`user_id`) REFERENCES  `go_users`(`ìd`) ON DELETE CASCADE
+) ENGINE = MyISAM DEFAULT CHARSET = utf8;
+
+-- -----------------------------------------------------
+-- Table `as_default_calendar`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `as_default_calendar`;
+CREATE TABLE IF NOT EXISTS `as_default_calendar` (
+  `user_id` INT(11) NOT NULL ,
+  `calendar_id` INT(11) NOT NULL ,
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`user_id`) REFERENCES  `go_users`(`ìd`) ON DELETE CASCADE,
+  FOREIGN KEY (`calendar_id`) REFERENCES  `cal_calendars`(`ìd`) ON DELETE CASCADE
+) ENGINE = MyISAM DEFAULT CHARSET = utf8;
+
+-- -----------------------------------------------------
+-- Table `as_calendars`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `as_calendars`;
+CREATE TABLE IF NOT EXISTS `as_calendars` (
+  `user_id` INT(11) NOT NULL ,
+  `calendar_id` INT(11) NOT NULL ,
+  PRIMARY KEY (`user_id`, `calendar_id`),
+  FOREIGN KEY (`user_id`) REFERENCES  `go_users`(`ìd`) ON DELETE CASCADE,
+  FOREIGN KEY (`calendar_id`) REFERENCES  `cal_calendars`(`ìd`) ON DELETE CASCADE
+) ENGINE = MyISAM DEFAULT CHARSET = utf8;
+
+-- -----------------------------------------------------
+-- Table `as_default_addressbook`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `as_default_addressbook`;
+CREATE TABLE IF NOT EXISTS `as_default_addressbook` (
+  `user_id` INT(11) NOT NULL ,
+  `addressbook_id` INT(11) NOT NULL ,
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`user_id`) REFERENCES  `go_users`(`ìd`) ON DELETE CASCADE,
+  FOREIGN KEY (`addressbook_id`) REFERENCES  `ab_addressbooks`(`ìd`) ON DELETE CASCADE
+) ENGINE = MyISAM DEFAULT CHARSET = utf8;
+
+-- -----------------------------------------------------
+-- Table `as_addressbooks`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `as_addressbooks`;
+CREATE TABLE IF NOT EXISTS `as_addressbooks` (
+  `user_id` INT(11) NOT NULL ,
+  `addressbook_id` INT(11) NOT NULL ,
+  PRIMARY KEY (`user_id`, `addressbook_id`),
+  FOREIGN KEY (`user_id`) REFERENCES  `go_users`(`ìd`) ON DELETE CASCADE,
+  FOREIGN KEY (`addressbook_id`) REFERENCES  `ab_addressbooks`(`ìd`) ON DELETE CASCADE
+) ENGINE = MyISAM DEFAULT CHARSET = utf8;
+
+-- -----------------------------------------------------
+-- Table `as_default_tasklist`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `as_default_tasklist`;
+CREATE TABLE IF NOT EXISTS `as_default_tasklist` (
+  `user_id` INT(11) NOT NULL ,
+  `tasklist_id` INT(11) NOT NULL ,
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`user_id`) REFERENCES  `go_users`(`ìd`) ON DELETE CASCADE,
+  FOREIGN KEY (`tasklist_id`) REFERENCES  `ta_lists`(`ìd`) ON DELETE CASCADE
+) ENGINE = MyISAM DEFAULT CHARSET = utf8;
+
+-- -----------------------------------------------------
+-- Table `as_tasklists`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `as_tasklists`;
+CREATE TABLE IF NOT EXISTS `as_tasklists` (
+  `user_id` INT(11) NOT NULL ,
+  `tasklist_id` INT(11) NOT NULL ,
+  PRIMARY KEY (`user_id`, `tasklist_id`),
+  FOREIGN KEY (`user_id`) REFERENCES  `go_users`(`ìd`) ON DELETE CASCADE,
+  FOREIGN KEY (`tasklist_id`) REFERENCES  `ta_lists`(`ìd`) ON DELETE CASCADE
+) ENGINE = MyISAM DEFAULT CHARSET = utf8;
--- install/uninstall.sql
+++ install/uninstall.sql
@@ -1,1 +1,7 @@
 DROP TABLE IF EXISTS `as_devices`;
+DROP TABLE IF EXISTS `as_default_calendar`;
+DROP TABLE IF EXISTS `as_calendars`;
+DROP TABLE IF EXISTS `as_default_addressbook`;
+DROP TABLE IF EXISTS `as_addressbooks`;
+DROP TABLE IF EXISTS `as_default_tasklist`;
+DROP TABLE IF EXISTS `as_tasklists`;
